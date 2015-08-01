8ffc18e58ed6e647a24e0caa6d918bad03593bdd
HarryNews <harrybrickner@gmail.com>
2015-08-01 15:57:44 -0400

    Artichoke V0.03b
    Made makePerson make a smaller person

--- src/util/BodyBuilder.java
+++ src/util/BodyBuilder.java
@@ -10,15 +10,15 @@ import bodies.BoundTree;
 public class BodyBuilder {
 	public static BoundTree makePerson(Point2D location, double size){
 		Node pelvis = new Node(location);
-		Node torso = new Node(0, 200 * size).lineProperties((float) (12 * size), Color.black).connectParent(pelvis);
-		DoubleNode thighs = new DoubleNode(165, 125 * size).lineProperties((float) (12 * size), Color.black).connectParent(pelvis);
-		DoubleNode shins = new DoubleNode(180, 125 * size).lineProperties((float) (12 * size), Color.black).connectParent(thighs);
-		DoubleNode feet = new DoubleNode(90, 40 * size).lineProperties((float) (10 * size), Color.black).connectParent(shins);
-		DoubleNode shoulders = new DoubleNode(100, 50 * size).lineProperties((float) (12 * size), Color.black).connectParent(torso);
-		DoubleNode bicep = new DoubleNode(165, 90 * size).lineProperties((float) (12 * size), Color.black).connectParent(shoulders);
-		DoubleNode forearm = new DoubleNode(180, 90 * size).lineProperties((float) (12 * size), Color.black).connectParent(bicep);
-		Node neck = new Node(0, 46 * size).lineProperties((float) (10 * size), Color.black).connectParent(torso);
-		Node head = new Node(0, 40 * size).lineProperties((float) (80 * size), Color.black).connectParent(neck);
+		Node torso = new Node(0, 50 * size).lineProperties((float) (3 * size), Color.black).connectParent(pelvis);
+		DoubleNode thighs = new DoubleNode(165, 32 * size).lineProperties((float) (3 * size), Color.black).connectParent(pelvis);
+		DoubleNode shins = new DoubleNode(180, 32 * size).lineProperties((float) (3 * size), Color.black).connectParent(thighs);
+		DoubleNode feet = new DoubleNode(90, 10 * size).lineProperties((float) (3 * size), Color.black).connectParent(shins);
+		DoubleNode shoulders = new DoubleNode(100, 13 * size).lineProperties((float) (3 * size), Color.black).connectParent(torso);
+		DoubleNode bicep = new DoubleNode(165, 24 * size).lineProperties((float) (3 * size), Color.black).connectParent(shoulders);
+		DoubleNode forearm = new DoubleNode(180, 24 * size).lineProperties((float) (3 * size), Color.black).connectParent(bicep);
+		Node neck = new Node(0, 11 * size).lineProperties((float) (3 * size), Color.black).connectParent(torso);
+		Node head = new Node(0, 10 * size).lineProperties((float) (20 * size), Color.black).connectParent(neck);
 		return new BoundTree(pelvis);
 	}
 }
